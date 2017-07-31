e55ec80ddacbef84f37c9df27e634d539635db18
Christopher <chrabyrd@gmail.com>
2017-07-31 16:26:28 -0700

    Add BST insert

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,5 +1,53 @@
 class Node
+  attr_reader :val
+  attr_accessor :left_child, :right_child
+
+  def initialize(val)
+    @val = val
+    @left_child = nil
+    @right_child = nil
+  end
 end
 
 class BinarySearchTree
+  attr_accessor :head
+
+  def initialize
+    @head = nil
+  end
+
+  def insert(val)
+    node = Node.new(val)
+
+    if @head.nil?
+      @head = node
+    else
+      insert_child_node(node, @head)
+    end
+  end
+
+  def insert_child_node(node, branch_node)
+    case node.val <=> branch_node.val
+    when 1
+      if branch_node.right_child != nil
+        insert_child_node(node, branch_node.right_child)
+      else
+        branch_node.right_child = node
+      end
+    else
+      if branch_node.left_child != nil
+        insert_child_node(node, branch_node.left_child)
+      else
+        branch_node.left_child = node
+      end
+    end
+  end
+
 end
+
+tree = BinarySearchTree.new
+tree.insert(6)
+tree.insert(7)
+tree.insert(7)
+
+p tree
