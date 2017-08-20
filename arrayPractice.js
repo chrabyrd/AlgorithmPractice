d0ea91479cbd4304fed5e761c494170bd3d37be6
Christopher <chrabyrd@gmail.com>
2017-08-20 11:29:52 -0700

    Add generateMatrix function

--- arrayPractice.js
+++ arrayPractice.js
@@ -18,3 +18,55 @@ const majorityElement = function(nums) {
 
     return parseInt(retKey);
 };
+
+const generateMatrix = function(n) {
+    if (n === 0) return [];
+
+    const retArr = [];
+    let count = Math.pow(n, 2);
+
+    const padLeft = () => {
+        if (count === 0) return;
+        retArr.forEach(subArr => subArr.unshift(count--));
+    };
+
+    const padBottom = () => {
+        if (count === 0) return;
+        const subArr = [];
+        while (subArr.length < retArr[0].length) {
+            subArr.push(count--);
+        }
+
+        retArr.push(subArr);
+    };
+
+    const padRight = () => {
+        if (count === 0) return;
+
+        retArr.forEach((_, idx) => retArr[(retArr.length - 1 - idx)].push(count--));
+    };
+
+    const padTop = () => {
+        if (count === 0) return;
+        const subArr = [];
+        while (subArr.length < retArr[0].length) {
+            subArr.unshift(count--);
+        }
+
+        retArr.unshift(subArr);
+    };
+
+    {
+        retArr.push([count]);
+        count--;
+    }
+
+    do {
+        padLeft();
+        padBottom();
+        padRight();
+        padTop();
+    } while (count > 0)
+
+    return (n % 2 === 0) ? retArr.map(subArr => subArr.reverse()).reverse() : retArr
+};
