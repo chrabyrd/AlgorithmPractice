afadb0f9576e9a9830ac00727cfccc157048bd9b
Christopher <chrabyrd@gmail.com>
2017-08-21 16:57:51 -0700

    Add string_practice.rb

--- string_practice.rb
+++ string_practice.rb
@@ -1,3 +1,24 @@
+def longest_palindrome(s)
+    hash = {}
+    longest_count = 0
+    remainder_exists = false
+
+    s.chars.each { |letter| hash[letter] ? hash[letter] += 1 : hash[letter] = 1 }
+
+    hash.keys.each do |key|
+
+        until hash[key] < 2
+            hash[key] -= 2
+            longest_count += 2
+        end
+
+        remainder_exists = true if remainder_exists == false && hash[key] == 1
+    end
+
+    remainder_exists ? longest_count + 1 : longest_count
+
+end
+
 def length_of_longest_substring(s)
     str_arr = s.chars
     hash = {}
